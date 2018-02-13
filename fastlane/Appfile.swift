363c2d6c907700253f6509b2b38e79504aa0bb0a
Dhamodaran Sri <dhamodaran.s@tridentnets.in>
2018-02-13 20:03:31 +0530

    first commit

---
+++ fastlane/Appfile.swift
@@ -0,0 +1,9 @@
+var appIdentifier: String { return "com.trident.hub" } // The bundle identifier of your app
+var appleID: String { return "support@mobilityminds.com" } // Your Apple email address
+
+var itcTeam: String? { return "318222" } // iTunes Connect Team ID
+var teamID: String { return "EA88Y3R72N" } // Apple Developer Portal Team ID
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
